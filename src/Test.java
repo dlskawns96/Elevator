4c04606fe031169f8f9a16d7056d4ee388f35c4b
유민 <dbals2007@naver.com>
2016-11-22 17:17:23 +0900

    잘부탁드립니당~~

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
 
 public class Test {
 	public static void main(String[] args) {
+		String NamJun_name;
+		NamJun_name = "NamJun";
 		System.out.println("HELLO NamJun im SungJae");
 		System.out.println("HELLO NamJun im Youmin *^^*");
 		System.out.println("Hello guys");
+		System.out.println("Hello" + NamJun_name);
 	}
 }
