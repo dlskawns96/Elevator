b4ad47405c4e9c04d6245440a935488559354358
dlskawns96@gmail.com <dlskawns96@gmail.com>
2016-11-22 15:35:42 +0900

    HI

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("HELLO NamJun im SungJae");
 		System.out.println("HELLO NamJun im Youmin *^^*");
-		
+		System.out.println("Hello guys");
 	}
 }
