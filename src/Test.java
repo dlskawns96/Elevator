64c92e7ebbcfc7220ebcce9b7df3ddf27fd8ca1f
유민 <dbals2007@naver.com>
2016-11-22 17:19:51 +0900

    NamJun_name implement

--- src/Test.java
+++ src/Test.java
@@ -6,14 +6,9 @@ public class Test {
 		System.out.println("HELLO NamJun im SungJae");
 		System.out.println("HELLO NamJun im Youmin *^^*");
 		System.out.println("Hello guys");
-<<<<<<< HEAD
 		System.out.println("Hello" + NamJun_name);
-=======
 		System.out.println("HaHaHa");
 		System.out.println("Go home");
 		System.out.println("No one");
-		
-		
->>>>>>> origin/master
 	}
 }
